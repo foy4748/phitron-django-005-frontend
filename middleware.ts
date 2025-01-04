0d5b2a3c7cb3fe4a67a9836a7eee4258015d0ca8
foy4748 <faisaljfcl@gmail.com>
2025-01-04 13:28:37 +0600

    Fixed Token Decoding at middleware at production

--- middleware.ts
+++ middleware.ts
@@ -1,7 +1,11 @@
 import { NextRequest, NextResponse } from "next/server";
 import { getToken } from "next-auth/jwt";
 export default async function midddleware(req: NextRequest) {
-  const token = await getToken({ req, secret: process.env.NEXTAUTH_SECRET });
+  const token = await getToken({
+    req,
+    secret: process.env.NEXTAUTH_SECRET,
+    secureCookie: process.env.NODE_ENV === "production" ? true : false,
+  });
   const isDashboard = req.nextUrl.pathname.startsWith("/dashboard");
   const isAdminRelatedRoute =
     req.nextUrl.pathname.startsWith("/dashboard/admin");
