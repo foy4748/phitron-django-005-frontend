75fdde7cdcc0cf5f0980ff9294660b2f8c0d49a6
foy4748 <faisaljfcl@gmail.com>
2025-01-10 01:02:32 +0600

    Converted ProductCardGrid to Client Component

--- app/products/components/ProductCardGridUseClient.tsx
+++ app/products/components/ProductCardGridUseClient.tsx
@@ -1,28 +1,41 @@
+"use client";
 import GridSystem from "@/components/customUI/GridSystem/GridSystem";
 import Col from "@/components/customUI/GridSystem/Col";
-import { ReadonlyURLSearchParams } from "next/navigation";
+import { useSearchParams } from "next/navigation";
 import { getProductList } from "@/actions/product/getProductList";
 import ProductCard from "@/app/products/components/ProductCard";
 import CategoryTitle from "@/app/products/components/CategoryTitle";
 import { TProductList } from "@/types/product";
 import ProductPagination from "@/app/products/components/Pagination";
+import { useEffect, useState } from "react";
+import Loading from "../loading";
 
 // Product Fetch Func
 
 // const repeat = (arr: TSingleProduct[], n: number) =>
 //   Array.from({ length: arr.length * n }, (_, i) => arr[i % arr.length]);
 
-export default async function ProductCardGrid({
-  searchParams,
-}: {
-  searchParams: Promise<ReadonlyURLSearchParams>;
-}) {
-  const s = await searchParams;
-  const strParams = JSON.stringify(s);
-  const params = JSON.parse(strParams);
-  params.limit = params?.limit || 12;
-  const queryStr = new URLSearchParams(params).toString();
-  const product_list: TProductList = await getProductList(queryStr);
+export default function ProductCardGridUseClient() {
+  const s = useSearchParams();
+  const [product_list, setProductList] = useState<TProductList>({
+    count: 0,
+    next: null,
+    previous: null,
+    results: [],
+  });
+  const [loading, setLoading] = useState(true);
+  useEffect(() => {
+    (async function () {
+      const strParams = JSON.stringify(s);
+      const params = JSON.parse(strParams);
+      params.limit = params?.limit || 12;
+      const queryStr = new URLSearchParams(params).toString();
+      const productList: TProductList = await getProductList(queryStr);
+      setProductList(productList);
+      setLoading(false);
+    })();
+  }, [s]);
+  if (loading) return <Loading cardsNumber={12} />;
   return (
     <>
       <h1 className="my-4">
@@ -38,7 +51,10 @@ export default async function ProductCardGrid({
             );
           })}
       </GridSystem>
-      <ProductPagination count={product_list.count} limit={params.limit} />
+      <ProductPagination
+        count={product_list.count}
+        limit={Number(s.get("limit")) || 12}
+      />
     </>
   );
 }
--- app/products/page.tsx
+++ app/products/page.tsx
@@ -1,27 +1,18 @@
-import { ReadonlyURLSearchParams } from "next/navigation";
-import { Suspense } from "react";
-import Loading from "./loading";
-import ProductCardGrid from "./components/ProductCardGrid";
 import { SearchAndFilterProduct } from "@/app/products/components/SearchAndFilterProduct";
+import ProductCardGridUseClient from "./components/ProductCardGridUseClient";
 
 // Product Fetch Func
 
 // const repeat = (arr: TSingleProduct[], n: number) =>
 //   Array.from({ length: arr.length * n }, (_, i) => arr[i % arr.length]);
 
-export default async function ProductCardPage({
-  searchParams,
-}: {
-  searchParams: Promise<ReadonlyURLSearchParams>;
-}) {
+export default async function ProductCardPage() {
   return (
     <>
-      <Suspense fallback={<Loading />}>
-        <div className="flex justify-end mt-4">
-          <SearchAndFilterProduct />
-        </div>
-        <ProductCardGrid searchParams={searchParams} />
-      </Suspense>
+      <div className="flex justify-end mt-4">
+        <SearchAndFilterProduct />
+      </div>
+      <ProductCardGridUseClient />
     </>
   );
 }
